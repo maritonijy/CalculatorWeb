e946bb69a7bed7b8f6f0691b1e8f7823b0d30bac
Jericho Magsino <jerichomagsino28@gmail.com>
2026-02-03 06:45:20 +0800

    I added nav-link for subtraction

--- CalculatorWeb/obj/Debug/net8.0/CalculatorWeb.AssemblyInfo.cs
+++ CalculatorWeb/obj/Debug/net8.0/CalculatorWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+375a91ef6940abed509a39f589ffe183e1400597")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f6ab54f3e0d5bed896b34da973915abc3a50174")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
